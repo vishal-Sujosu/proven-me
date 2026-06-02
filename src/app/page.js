a2a676d5ff1ff491046f0c19ed7d3fdc1dca7735
vishal-sujosu <vishaljadhav@sujosu.com>
2026-06-02 14:22:31 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,11 @@
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.js file.
@@ -60,6 +54,9 @@ export default function Home() {
           </a>
         </div>
       </main>
+      <Button variant="outline" size="lg">
+        Hello World
+      </Button>
     </div>
   );
 }
